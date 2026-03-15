bc8939912cbe2f6a2545fb2eb7c52f6c176506c6
vamsimudadla <mudadlavamsi@gmail.com>
2026-03-15 10:05:26 +0530

    Sass setup

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
-})
+  modules: ["@nuxt/eslint", "@nuxt/image"],
+  css: ["~/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/styles/variables" as *;`,
+        },
+      },
+    },
+  },
+});
